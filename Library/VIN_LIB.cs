761acf25281ce1b5fbf22837ec38daca9ac87bfd
МатвеевИС <@211-11>
2023-02-20 14:13:52 +0300

    Добавьте файлы проекта.

---
+++ Library/VIN_LIB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public  class VIN_LIB
+    {
+        public static bool CheckVIN(string vin)
+        {
+            Regex regex = new Regex("^[A-HJ-NRT-Z0-9]{17}$"); // регулярное выражение 
+            bool a = regex.IsMatch(vin);
+            if(!a)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        //public string GetVINCountry(string vin)
+        //{
+        //    string countre;
+        //    return countre;
+        //}
+
+    }
+}
---
+++ UnitTest/Unit-tests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Library;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod] //1
+        public void CheckingForARegularExpression1()
+        {
+            string vin = "YS3AL76L1R7002116";
+            bool Checkin = VIN_LIB.CheckVIN(vin);
+            Assert.AreNotEqual(Checkin, true);
+        }
+       
+
+        [TestMethod] //2
+        public void CheckingForAnEmptyValue()
+        {
+            string vin = "JH4KA4540LC016127";
+            Assert.IsNotNull(vin);
+        }
+
+
+        [TestMethod] //3
+        public void CheckingForAnEmptyValue2()
+        {
+            string vin = null;
+            Assert.IsNull(vin);
+        }
+    }
+}
