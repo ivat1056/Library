6c5b11a1bf6abb46cfe286907365e01e60f2a7b6
иван матвеев <ivat1056@gmail.com>
2023-02-20 15:01:10 +0300

    12

--- UnitTest/Unit-tests.cs
+++ UnitTest/Unit-tests.cs
@@ -8,7 +8,7 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod] //1
-        public void CheckingForARegularExpression1()
+        public void CheckingForARegularExpression1() // простой
         {
             string vin = "YS3AL76L1R7002116";
             bool Checkin = VIN_LIB.CheckVIN(vin);
@@ -16,14 +16,14 @@ namespace UnitTest
         }
 
         [TestMethod] //2
-        public void CheckingForARegularExpression2()
+        public void CheckingForARegularExpression2() // простой
         {
             string vin = "11A5513P123137478";
             bool Checkin = VIN_LIB.CheckVIN(vin);
             Assert.IsFalse(Checkin);
         }
         [TestMethod] //3
-        public void CheckingForAnEmptyValue()
+        public void CheckingForAnEmptyValue() // сложные
         {
             string vin = "JH4KA4540LC016127";
             Assert.IsNotNull(vin);
@@ -31,10 +31,19 @@ namespace UnitTest
 
 
         [TestMethod] //4
-        public void CheckingForAnEmptyValue2()
+        public void CheckingForAnEmptyValue2() // сложные
         {
             string vin = null;
             Assert.IsNull(vin);
         }
+        [TestMethod] //5
+        public void CheckingType() // сложный 
+        {
+            string vin = "2FMDA5146TBC22506";
+            bool Checkin = VIN_LIB.CheckVIN(vin);
+            Assert.IsInstanceOfType(Checkin, typeof(bool));
+        }
+        [TestMethod] //6
+        
     }
 }
